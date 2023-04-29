b5162ee056a50f7335aff97538b976bf791d13c9
Jennifer-spec <fuqinghua2@gmail.com>
2023-04-28 17:51:24 -0700

    new file:   Basic JavaScript Projects/Project1_expressions_alert/JS/main.js and index.html

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,28 @@
+//alert("Hello,world!");
+/* window.alert("Hello hello,World!");  */
+//document.write("Hello world!");
+var X="Hello, Jennifer!";
+document.write(X);
+
+var Y="I don\'t know what he said.\" Hello\"";
+document.write(Y);
+
+document.write("\"Be who you are and say what you feel," + " because those who mind don\'t matter and those who matter don\'t mind.\""+ "-Dr. Seuss");
+
+var Z="John"+ " Smith";
+document.write(Z);
+
+var Family="The Arezzinis", Dad="Jeremiah", Mom="Hermoine", Daughter ="Penny", Son="zorro";
+
+document.write(Mom);
+
+document.write(3+3);
+
+function My_First_Function() {
+    var str = "This is the button text"
+    document.getElementById("Button_Text").innerHTML = str;
+}
+
+var Sent1="This is the beginning of the string";
+var Sent2="and this is the end of the string";
+document.write(Sent1+Sent2);
